262db359b215b3ec4af774a72e88713cee39d417
조병우 <whquddn55@gmail.com>
2022-10-14 23:49:54 +0900

    Fix: 서브 프로젝트 삭제

--- BlockChain/README.md
+++
@@ -1,3 +0,0 @@
-# AOMD-HyperLedger
-2022 JO2SEO팀 졸업과제
-하이퍼레저 페브릭 레포
--- FrontEnd/src/domainBox.js
+++
@@ -1,4 +0,0 @@
-export const KakaoRedirectURL = 'http://20.249.85.3:80/api/v1/auth/kakao';
-export const ServerURL = 'http://20.249.85.3';
-export const TestURL = 'http://172.30.1.9:8080/api/v1/portfolio/block';
-export const TestLoginURL = 'http://172.30.1.9:8080/api/v1/auth/login';
